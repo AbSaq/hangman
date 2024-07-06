6ff8f516cc04f7aa452fcb839c3fa516d524e9c6
DhoAq <shoomt.akeel@gmail.com>
2024-07-06 21:50:17 +0300

    feat: start

---
+++ hangman/main.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int len_of_str(char *str) {
+    int i = 0;
+    while (1) {
+        if (str[i] == '\0') {
+            return i;
+        }
+        i++;
+    }
+}
+
+int has_char(char str[], char c) {
+    int len = len_of_str(str);
+    for (int i = 0; i<len; i++) {
+        if (str[i] == c) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+char* set_empty_guess(char *str) {
+    int len = len_of_str(str);
+    printf("%d", len);
+    for (int i = 1; i<len; i++) {
+        str[i] = '_';
+    }
+    return str;
+}
+
+
+void update_progress(char *original_word, char *empty_word, char found_char) {
+    //printf("update_progress (%s) (%s) (%c)\n", original_word, empty_word, found_char);
+    for (int i = 0; i<len_of_str(original_word); i++) {
+        if (original_word[i] == found_char)
+            empty_word[i] = found_char;
+    }
+    //printf("update_progress2 (%s) (%s) (%c)\n", original_word, empty_word, found_char);
+
+}
+
+
+char get_char(void) {
+    char ch = ' ';
+    while (1) {
+        char input = ' ';
+        scanf("%c", &input);
+        if (input == 10) {
+            break;
+        }
+        ch = input;
+    }
+    return ch;
+}
+
+int main() {
+    int won = 0;
+    short int attempts = 5;
+    char word_to_guess[] = "dread";
+    int len = len_of_str(word_to_guess);
+
+    char progress_word[len+1];
+    for (int i =0; i<len_of_str(word_to_guess); i++) {
+        progress_word[i] = '_';
+    }
+    progress_word[len] = '\0';
+
+
+    while (attempts > 0 | won) {
+        char guessed_character = get_char();
+
+        int guessed_right = has_char(word_to_guess, guessed_character);
+
+        if (guessed_right) {
+            update_progress(word_to_guess, progress_word, guessed_character);
+            printf("correct! %s\n", progress_word);
+
+            int finished = has_char(progress_word, '_');
+            if (!finished) {
+                won = 1;
+                break;
+            }
+
+        }
+        else { // guessed wrong
+            attempts--;
+            printf("false! %d/5 attempts left.\n", attempts);
+        }
+    }
+
+    if (won) {
+        printf("congrats, you have won.\n");
+    }
+    else {
+        printf("thumb_down");
+    }
+
+}
+
